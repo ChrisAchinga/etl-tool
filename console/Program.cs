30a3bdfa73f0c6fe2f0eaa159f895ea0c8205f63
ChrisAchinga <achinga.chris@gmail.com>
2021-07-06 18:30:57 +0300

    read non-encrypted tsv file and display content

---
+++ console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+
+namespace etl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                String st = File.ReadAllText("../data/sample.tsv");
+                Console.WriteLine(st);
+            }
+            catch (System.Exception e)
+            {
+                
+                Console.WriteLine("This file cannot be read");
+                Console.WriteLine(e.Message);
+            }
+            Console.Read();
+        }
+    }
+}
