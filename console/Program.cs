e108f979b62e232147d589509a4ba681ffe9de0c
ChrisAchinga <achinga.chris@gmail.com>
2021-07-20 11:41:01 +0300

    function: to get file and decode

--- console/Program.cs
+++ console/Program.cs
@@ -9,36 +9,46 @@ namespace etl
     {
         static void Main(string[] args)
         {
+            // creating a function to get the files and encode them
+            static void getFileDecryption(string  fileName) {
+                var encryptFile = File.ReadAllText(fileName);
+                byte[] decryptFile = Convert.FromBase64String(encryptFile);
+                string decodedFile = Encoding.Unicode.GetString(decryptFile);
+                Console.WriteLine(decodedFile);
+                Console.WriteLine("Testing a Function");
+            }
+            getFileDecryption("../data/companies-encrypted.tsv");
+            getFileDecryption("../data/devices-encrypted.tsv");
             // geting the files and encoding
-            try
-            {
-                // file paths
-                var companyFile = "../data/companies-encrypted.tsv";
-                var deviceFile = "../data/devices-encrypted.tsv";
+            // try
+            // {
+            //     var companyFile = "../data/companies-encrypted.tsv";
+            //     var deviceFile = "../data/devices-encrypted.tsv";
 
-                var companyEncrypted = File.ReadAllText(companyFile);
-                var deviceEncrypted = File.ReadAllText(deviceFile);
+            //     var companyEncrypted = File.ReadAllText(companyFile);
+            //     var deviceEncrypted = File.ReadAllText(deviceFile);
                 
-                byte[] companyDecrypted = Convert.FromBase64String(companyEncrypted);
-                byte[] deviceDecrypted = Convert.FromBase64String(deviceEncrypted);
+            //     byte[] companyDecrypted = Convert.FromBase64String(companyEncrypted);
+            //     byte[] deviceDecrypted = Convert.FromBase64String(deviceEncrypted);
 
-                string companyDecoded = Encoding.Unicode.GetString(companyDecrypted);
-                string deviceDecoded = Encoding.Unicode.GetString(deviceDecrypted);
+            //     string companyDecoded = Encoding.Unicode.GetString(companyDecrypted);
+            //     string deviceDecoded = Encoding.Unicode.GetString(deviceDecrypted);
 
-                Console.WriteLine("Company Data");
-                Console.WriteLine(companyDecoded);
+            //     Console.WriteLine("Company Data");
+            //     Console.WriteLine(companyDecoded);
 
-                Console.WriteLine("Device Data");
-                Console.WriteLine(deviceDecoded);
+            //     Console.WriteLine("Device Data");
+            //     Console.WriteLine(deviceDecoded);
 
-            }
-            catch (System.Exception e)
-            {
+            // }
+            // catch (System.Exception e)
+            // {
                 
-                Console.WriteLine("This file cannot be read");
-                Console.WriteLine(e.Message);
-            }
+            //     Console.WriteLine("This file cannot be read");
+            //     Console.WriteLine(e.Message);
+            // }
 
+            // file parsing
             try
             {
                 Console.WriteLine("Creating MySQL Database");
