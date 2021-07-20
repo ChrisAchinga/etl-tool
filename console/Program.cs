fc1526d3bdeca16aedeff96781f14c99b20688bc
ChrisAchinga <achinga.chris@gmail.com>
2021-07-20 11:48:19 +0300

    clean code

--- console/Program.cs
+++ console/Program.cs
@@ -10,54 +10,25 @@ namespace etl
         static void Main(string[] args)
         {
             // creating a function to get the files and encode them
-            static void getFileDecryption(string  fileName) {
-                var encryptFile = File.ReadAllText(fileName);
-                byte[] decryptFile = Convert.FromBase64String(encryptFile);
-                string decodedFile = Encoding.Unicode.GetString(decryptFile);
-                Console.WriteLine(decodedFile);
-                Console.WriteLine("Testing a Function");
-            }
-            getFileDecryption("../data/companies-encrypted.tsv");
-            getFileDecryption("../data/devices-encrypted.tsv");
-            // geting the files and encoding
-            // try
-            // {
-            //     var companyFile = "../data/companies-encrypted.tsv";
-            //     var deviceFile = "../data/devices-encrypted.tsv";
-
-            //     var companyEncrypted = File.ReadAllText(companyFile);
-            //     var deviceEncrypted = File.ReadAllText(deviceFile);
-                
-            //     byte[] companyDecrypted = Convert.FromBase64String(companyEncrypted);
-            //     byte[] deviceDecrypted = Convert.FromBase64String(deviceEncrypted);
-
-            //     string companyDecoded = Encoding.Unicode.GetString(companyDecrypted);
-            //     string deviceDecoded = Encoding.Unicode.GetString(deviceDecrypted);
-
-            //     Console.WriteLine("Company Data");
-            //     Console.WriteLine(companyDecoded);
-
-            //     Console.WriteLine("Device Data");
-            //     Console.WriteLine(deviceDecoded);
-
-            // }
-            // catch (System.Exception e)
-            // {
-                
-            //     Console.WriteLine("This file cannot be read");
-            //     Console.WriteLine(e.Message);
-            // }
-
-            // file parsing
             try
             {
-                Console.WriteLine("Creating MySQL Database");
+                static void getFileDecryption(string  fileName) {
+                    var encryptFile = File.ReadAllText(fileName);
+                    byte[] decryptFile = Convert.FromBase64String(encryptFile);
+                    string decodedFile = Encoding.Unicode.GetString(decryptFile);
+
+                    Console.WriteLine(decodedFile);
+                }
+                getFileDecryption("../data/companies-encrypted.tsv");
+                getFileDecryption("../data/devices-encrypted.tsv");
+                
             }
             catch (System.Exception e)
             {
                 Console.WriteLine("Database operations failed");
                 Console.WriteLine(e.Message);
             }
+            
             Console.Read();
         }
     }
