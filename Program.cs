5bf434944a42866bc769d525cc0d9f2532b38fa2
ChrisAchinga <achinga.chris@gmail.com>
2021-09-04 09:58:38 +0300

    file parse attempt

--- Program.cs
+++ Program.cs
@@ -1,34 +1,27 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 
 namespace etl
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            // creating a function to get the files and encode them
-            try
-            {
-                static void getFileDecryption(string  fileName) {
-                    var encryptFile = File.ReadAllText(fileName);
-                    byte[] decryptFile = Convert.FromBase64String(encryptFile);
-                    string decodedFile = Encoding.Unicode.GetString(decryptFile);
-                    Console.WriteLine(decodedFile);
-                }
-                getFileDecryption("./data/companies-encrypted.tsv");
-                getFileDecryption("./data/devices-encrypted.tsv");
-                
-            }
-            catch (System.Exception e)
-            {
-                Console.WriteLine("File operations failed");
-                Console.WriteLine(e.Message);
-            }
-            
+            var encryptFile = File.ReadAllText("./data/devices-encrypted.tsv");
+            byte[] decryptFile = Convert.FromBase64String(encryptFile);
+            string decodedFile = Encoding.Unicode.GetString(decryptFile);
+            File.WriteAllText("./data/devices.tsv", decodedFile);
+            Console.WriteLine(decodedFile);
+
+            var lines = File.ReadAllLines("./data/devices.tsv");
+            var csv = lines.Select(row => string.Join(",", Format(row).Split('\t')));            
             Console.Read();
         }
     }
 }
+
+// replace tabs with comas, make it csv
+// parse csv
